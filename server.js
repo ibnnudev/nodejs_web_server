3f0bed748e096cde2e24fcd995433f186221790b
Moh Ibnu Abdurrohman Sutio <ibnuabdurrohmansutio@gmail.com>
2025-06-16 13:57:44 +0700

    Add initial data files: JSON and TXT
    - Created data.json with sample user data (firstname and lastname).
    - Created data.txt with placeholder text.

--- server.js
+++ server.js
@@ -1,27 +1,38 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const { logger } = require("./middleware/logEvents");
 const PORT = process.env.PORT || 3500;
+const cors = require("cors");
+
+// CORS middleware
+const whitelist = ["https://www.google.com", "http://localhost:3500"];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+// custom middleware logger
+app.use(logger);
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
 });
-app.get("/new-page", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "new-page.html"));
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send(err.message);
 });
 
-const one = (req, res, next) => {
-  console.log("one");
-  next();
-};
-const two = (req, res, next) => {
-  console.log("two");
-  next();
-};
-const three = (req, res) => {
-  console.log("three");
-  console.log("Finished!");
-};
-
-app.get("/chain", one, two, three);
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
